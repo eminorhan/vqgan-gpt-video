2579c1af0438b18dd007a8694c09a04a3e5688d9
eminorhan <aeminorhan@gmail.com>
2023-09-24 23:26:41 -0400

    add scripts for new vqgan model configs + delete old ones

--- scripts/train_vqgan_256_16x16x16.sh
+++ scripts/train_vqgan_256_16x16x16.sh
@@ -6,8 +6,8 @@
 #SBATCH --cpus-per-task=16
 #SBATCH --mem=480GB
 #SBATCH --time=48:00:00
-#SBATCH --job-name=train_vqgan_256_32x32
-#SBATCH --output=train_vqgan_256_32x32_%A_%a.out
+#SBATCH --job-name=train_vqgan_256_16x16x16
+#SBATCH --output=train_vqgan_256_16x16x16_%A_%a.out
 #SBATCH --array=0
 
 export MASTER_ADDR=$(hostname -s)
@@ -18,23 +18,23 @@ srun python -u ../train_vqgan.py \
     --embedding_dim 256 \
     --n_codes 16384 \
     --n_hiddens 32 \
-    --downsample 4 8 8 \
+    --downsample 1 16 16 \
     --no_random_restart \
     --gpus 4 \
     --sync_batchnorm \
     --batch_size 4 \
     --num_workers 16 \
-    --accumulate_grad_batches 6 \
+    --accumulate_grad_batches 8 \
     --progress_bar_refresh_rate 1000 \
     --max_steps 2000000 \
     --gradient_clip_val 1.0 \
     --lr 0.00003 \
     --data_path "/scratch/eo41/data-video/minute/S" \
-    --default_root_dir "/scratch/eo41/vqgan-gpt-video/models_256_32x32" \
+    --default_root_dir "/scratch/eo41/vqgan-gpt-video/models_256_16x16x16" \
     --resolution 256 \
     --sequence_length 16 \
     --frame_rate 8 \
-    --discriminator_iter_start 20000 \
+    --discriminator_iter_start 16000 \
     --norm_type batch \
     --perceptual_weight 4 \
     --image_gan_weight 1 \
